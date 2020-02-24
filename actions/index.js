cb042e5e0cc710416c85461ed287ed60f52bf69b
Goutam A S <goutamas.keladi@gmail.com>
2020-02-24 09:31:15 +0530

    Redux toolkit added

--- actions/index.js
+++ actions/index.js
@@ -1,12 +1,17 @@
 import { INCREMENT, DECREMENT, INPUT_CHANGE } from '../constants/actionTypes';
+import { createAction } from '@reduxjs/toolkit';
 
-export const onIncrementClick = () => ({
-	type: INCREMENT
-});
+// export const onIncrementClick = () => ({
+// 	type: INCREMENT
+// });
 
-export const onDecrementClick = () => ({
-	type: DECREMENT
-});
+// export const onDecrementClick = () => ({
+// 	type: DECREMENT
+// });
+
+export const onIncrementClick = createAction(INCREMENT);
+
+export const onDecrementClick = createAction(DECREMENT);
 
 export const inputChanged = (e) => ({
 	type: INPUT_CHANGE,
--- reducers/counter.js
+++ reducers/counter.js
@@ -1,24 +1,42 @@
-import { INCREMENT, DECREMENT } from '../constants/actionTypes';
+import { onIncrementClick, onDecrementClick } from '../actions';
+import { createReducer } from '@reduxjs/toolkit';
 
 const initialState = {
 	count: 3
 };
 
-const counter = (state = initialState, action) => {
-	switch (action.type) {
-		case 'INCREMENT':
-			return {
-				...state,
-				count: state.count + 1
-			};
-		case 'DECREMENT':
-			return {
-				...state,
-				count: state.count - 1
-			};
-		default:
-			return state;
+const counter = createReducer(initialState, {
+	// [onIncrementClick]: (state) => state.count + 1,
+	// [onDecrementClick]: (state) => state.count - 1
+	[onIncrementClick]: (state, action) => {
+		return {
+			...state,
+			count: state.count + 1
+		};
+	},
+	[onDecrementClick]: (state, action) => {
+		return {
+			...state,
+			count: state.count - 1
+		};
 	}
-};
+});
+
+// const counter = (state = initialState, action) => {
+// 	switch (action.type) {
+// 		case 'INCREMENT':
+// 			return {
+// 				...state,
+// 				count: state.count + 1
+// 			};
+// 		case 'DECREMENT':
+// 			return {
+// 				...state,
+// 				count: state.count - 1
+// 			};
+// 		default:
+// 			return state;
+// 	}
+// };
 
 export default counter;
